92037d247038359a85c4836955d6a3f40c3fce1d
Pardeep Kaur <deepnabha3244@gmail.com>
2026-03-13 15:01:32 -0400

    add jenkins file

--- jenkins.sh
+++ jenkins.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-echo "Updating system..."
-sudo apt update
-
 echo "Installing Java..."
-sudo apt install openjdk-17-jdk -y
-
-echo "Adding Jenkins repository..."
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-/usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-echo "Adding Jenkins source list..."
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
-
-echo "Updating again..."
 sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
 
 echo "Installing Jenkins..."
+
+sudo mkdir -p /etc/apt/keyrings
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc 
+https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | 
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+
 sudo apt install jenkins -y
 
-echo "Starting Jenkins..."
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+echo "Jenkins installed successfully"
 
-echo "Jenkins installed successfully!"
-echo "Open: http://localhost:8080"
-
-echo "Initial password:"
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
